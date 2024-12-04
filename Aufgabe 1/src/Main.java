0cfad4db84f0fbe6a4110114ddec3f0d13707bb0
Marvin Jäger <marvin467@schule.bremen.de>
2024-12-04 11:31:22 +0100

    Aufgaben

--- Aufgabe 1/src/Main.java
+++ Aufgabe 1/src/Main.java
@@ -1,5 +1,6 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main { //Klasse erstellt mit den Namen "Main"
+    public static void main(String[] args) { //Main erstellt um auf die Konsole was zu bekommen
+
+        System.out.println("Hello world!"); //Gebe auf der Konsole "Hello world" aus
     }
 }
---
+++ Checkliste/src/DoubleTest.java
@@ -0,0 +1,6 @@
+public class DoubleTest {
+    public static void main(String[] args) {
+        double x = 32; // x als double "32" deklariert
+        System.out.println("Ein Double: " + x); // x auf der Konsole ausgegeben
+    }
+}
