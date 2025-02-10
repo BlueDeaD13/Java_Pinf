5cecc231627c7d1cded2cea694ebf518f0e8b3ff
Marvin Jäger <marvin467@schule.bremen.de>
2025-02-10 11:08:13 +0100

    Aufgaben

--- Objektorientierung/Klassendesign/src/Gameplay.java
+++ Objektorientierung/Klassendesign/src/Gameplay.java
@@ -1,12 +1,47 @@
+import java.util.Scanner;
+
 public class Gameplay {
-    private static Spielfeld feld = new Spielfeld();
-    private String[] spielernamen;
+    private static String[] spielernamen = new String[2];
+    private static Spielfeld feld;
 
-    public int spaltenAbfrage(int spielerID){
+    public static void init() {
+        feld = new Spielfeld();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name von Spieler 1 (X): ");
+        spielernamen[0] = scanner.nextLine();
+        System.out.print("Name von Spieler 2 (O): ");
+        spielernamen[1] = scanner.nextLine();
+    }
 
+    public static int spaltenAbfrage(int spielerID) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print(spielernamen[spielerID] + ", wähle eine Spalte (1-7) oder 0 zum Beenden: ");
+            int spalte = scanner.nextInt();
+            if (spalte == 0) {
+                System.out.println("Spiel wird beendet.");
+                return 0;
+            } else if (spalte >= 1 && spalte <= 7) {
+                return spalte;
+            } else {
+                System.out.println("Ungültige Eingabe! Bitte 1-7 oder 0 eingeben.");
+            }
+        }
     }
 
     public static void main(String[] args) {
-        feld.anzeigen();
+        init();
+        int aktuellerSpieler = 0;
+        while (true) {
+            feld.anzeigen();
+            int spalte = spaltenAbfrage(aktuellerSpieler);
+            if (spalte == 0) break;
+
+            if (feld.steinEinwerfen(spalte, aktuellerSpieler)) {
+                aktuellerSpieler = 1 - aktuellerSpieler; // Spielerwechsel
+            } else {
+                System.out.println("Fehler: Spalte nicht verfügbar!");
+            }
+        }
     }
 }
--- Objektorientierung/Klassendesign/src/Spielfeld.java
+++ Objektorientierung/Klassendesign/src/Spielfeld.java
@@ -1,59 +1,51 @@
 public class Spielfeld {
-    // Konstanten
-    private static final byte SPALTEN = 7;
-    private static final byte ZEILEN = 6;
-    private static final char LEERES_SYMBOL = '◌';
-    private static final char SYMBOL_1 = '●';
-    private static final char SYMBOL_2 = '○';
+    private char[][] spielsteine;
+    private static final int ZEILEN = 6;
+    private static final int SPALTEN = 7;
+    private static final char LEER = '-';
+    private static final char SPIELER1 = 'X';
+    private static final char SPIELER2 = 'O';
 
-    // Attribute
-    private char[][] spielsteine = new char[ZEILEN][SPALTEN];
-
-    // Konstruktor
     public Spielfeld() {
-        for (int zeile = 0; zeile < ZEILEN; zeile++) {
-            for (int spalte = 0; spalte < SPALTEN; spalte++) {
-                spielsteine[zeile][spalte] = LEERES_SYMBOL;
+        spielsteine = new char[ZEILEN][SPALTEN];
+        for (int i = 0; i < ZEILEN; i++) {
+            for (int j = 0; j < SPALTEN; j++) {
+                spielsteine[i][j] = LEER;
             }
         }
     }
 
-    // Methoden
     public void anzeigen() {
-        for (int zeile = 0; zeile < ZEILEN; zeile++) {
-            for (int spalte = 0; spalte < SPALTEN; spalte++) {
-                System.out.print(spielsteine[zeile][spalte] + " ");
+        for (int i = 0; i < ZEILEN; i++) {
+            for (int j = 0; j < SPALTEN; j++) {
+                System.out.print(spielsteine[i][j] + " ");
             }
             System.out.println();
         }
+        for (int j = 1; j <= SPALTEN; j++) {
+            System.out.print(j + " ");
+        }
+        System.out.println("\n");
     }
 
     public int findeFreienPlatz(int spalte) {
-        // Überprüft von unten nach oben die erste freie Zeile in der Spalte
-        for (int zeile = ZEILEN - 1; zeile >= 0; zeile--) {
-            if (spielsteine[zeile][spalte] == LEERES_SYMBOL) {
-                return zeile;
+        if (spalte < 1 || spalte > SPALTEN) {
+            System.out.println("Fehler: Ungültige Spalte!");
+            return -1;
+        }
+        for (int i = ZEILEN - 1; i >= 0; i--) {
+            if (spielsteine[i][spalte - 1] == LEER) {
+                return i;
             }
         }
-        return -1; // Gibt -1 zurück, wenn die Spalte voll ist
+        System.out.println("Fehler: Spalte ist voll!");
+        return -1;
     }
 
-    public boolean steinWerfen(int spalte, int spielerID) {
-        // Überprüft, ob die Spalte gültig ist
-        if (spalte < 0 || spalte >= SPALTEN) {
-            System.out.println("Ungültige Spalte. Bitte wähle eine Spalte zwischen 0 und " + (SPALTEN - 1));
-            return false;
-        }
-
-        // Findet die erste freie Zeile
-        int freieZeile = findeFreienPlatz(spalte);
-        if (freieZeile == -1) {
-            System.out.println("Die Spalte " + spalte + " ist voll. Wähle eine andere Spalte.");
-            return false;
-        }
-
-        // Legt den Stein basierend auf der Spieler-ID in die Spalte
-        spielsteine[freieZeile][spalte] = (spielerID == 1) ? SYMBOL_1 : SYMBOL_2;
+    public boolean steinEinwerfen(int spalte, int spielerID) {
+        int zeile = findeFreienPlatz(spalte);
+        if (zeile == -1) return false;
+        spielsteine[zeile][spalte - 1] = (spielerID == 0) ? SPIELER1 : SPIELER2;
         return true;
     }
 }
